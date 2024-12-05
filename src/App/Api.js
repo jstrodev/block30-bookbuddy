f6c6f16ed00d67509ab63ec54ef706a131381ce7
SavageLina <rios.paulinabelen@gmail.com>
2024-12-05 09:08:34 -0600

    bookslice changes

--- src/App/Api.js
+++ src/App/Api.js
@@ -3,7 +3,7 @@ import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api",
+    baseUrl: "https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api/books",
     prepareHeaders: (headers) => {
       const token = localStorage.getItem("token");
       if (token) {
--- src/components/Books/Books.jsx
+++ src/components/Books/Books.jsx
@@ -1,6 +1,6 @@
 /* TODO - add your code to create a functional React component that displays all of the available books in the library's catalog. Fetch the book data from the provided API. Users should be able to click on an individual book to navigate to the SingleBook component and view its details. */
 import React from 'react';
-import { useGetBooksQuery } from "./BooksSlice"
+import { useGetBooksQuery } from "./bookSlice"
 
 const Books = () => {
   const { data: books, isLoading, isSuccess } = useGetBooksQuery();
